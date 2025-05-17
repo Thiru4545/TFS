fed3f1e4c97d406fe28298df1e53220b4fa99a66
Boopalan.G <thirubalan3599@gmail.com>
2025-05-18 04:43:47 +0530

    Add files via upload

---
+++ CreateInstances.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0-beta"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "TG"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-2"
+}
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+}
