40d57b2682ec7743e3965b3144a87046ccc30173
Boopalan.G <thirubalan3599@gmail.com>
2025-06-16 18:55:36 +0530

    Update createinstances.tf

--- Day 01/createinstances.tf
+++ Day 01/createinstances.tf
@@ -1,19 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.0.0"
-    }
-  }
+# General settings
+aws_region   = "us-east-1"
+project_name = "demo"
+environment  = "dev"
+
+tags = {
+  Environment = "dev"
+  Project     = "booo"
 }
 
-provider "aws" {
-  access_key = "AKIAZ24ISOK6LAFT324F"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-2"
-}
-
-resource "aws_instance" "MyFirstInstance" {
-  ami           = "ami-0b0ea68c435eb488d"
-  instance_type = "t2.micro"
-}
+# EC2 settings
+web_server_ami           = "ami-084568db4383264d4"
+web_server_instance_type = "t2.micro"
+ssh_key_name             = "Terrraform"
+root_volume_type         = "gp2"
+root_volume_size         = 8
+iam_instance_profile     = null
+subnet_id_ec2            = "subnet-0aade558b97319d86"
+security_group_ids       = ["sg-09a1e6e668e0e62df"]
+# admin_ip_cidr            = "0.0.0.0/0"
